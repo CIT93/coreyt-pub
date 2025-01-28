b23658d8e62b767b0c11e79e77168739d687a1ba
CThomas9297 <theund3rgroundmetro@gmail.com>
2025-01-28 05:54:33 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log("week 1-Hello world");
+console.log("hello from inside the main.js file"); 
+
+// let myVar;
+
+const myVar = 42;
+const myvarType =typeof(myVar);
+console.log("myvarType" + myvarType);
+console.log(`myvarType ${myvarType}`);
+
+     
+   
+ function runNow()
+if(myvarType=== "number")
+    console.log('will 11 this one run?');
+  else
+    console.log(`will 13 this one run?`);
+
+
+runNow()
+runNow()
+      
