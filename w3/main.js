85fef4d0be0e35f328e5841e48ac3fc6d6fb7929
CThomas9297 <theund3rgroundmetro@gmail.com>
2025-01-29 06:55:27 +0000

    Template Strings

--- w3/main.js
+++ w3/main.js
@@ -21,3 +21,14 @@ console.log(typeof myVar);
 
 const myHeading =document.querySelector("h1");
 sole.log(typeof myHeading)
+
+const myPet= "cat"
+const myPetname = "Monkey";
+const myPetAge = 2
+
+console.log ("I have a" + myPet +" her name is " + myPetname + " and she is" + myPetAge +" years old "
+const outputString=`I have a ${myPet} her name is ${myPetName}and she is ${myPetAge}`
+myHeading.textContent= outputString
+
+
+// Dont know
